135722a6dff8468beb307f867b4d7589eeb93ed2
davidblogger <davidblogger@gmail.com>
2023-10-15 13:46:19 -0400

    Ejemplo de Sitio Web con React y TailwindCSS

---
+++ reto-frontend/src/components/NewArticle.jsx
@@ -0,0 +1,10 @@
+export const NewArticle = () => {
+  return (
+    <article className="h-[140px] border-b-2 border-GrayishBlue py-7 last:border-none">
+      <h2 className="cursor-pointer hover:text-SoftOrange">
+        Hydrogen VS Electric Cars
+      </h2>
+      <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+    </article>
+  );
+};
